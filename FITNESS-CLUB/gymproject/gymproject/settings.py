8a9198d544bbd20d9239ecbba9da4eb2686baf3f
vaishnvich45-dotcom <vaishnvich45@gmail.com>
2026-05-11 01:11:08 +0530

    Update settings.py

--- FITNESS-CLUB/gymproject/gymproject/settings.py
+++ FITNESS-CLUB/gymproject/gymproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xd%z%m*uzu$yd7)oa(6n3a3^bg0e*1nxu+o6rshp@inc=4shk&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
